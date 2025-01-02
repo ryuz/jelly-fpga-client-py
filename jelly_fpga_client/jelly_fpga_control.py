88d73cdf6e80ca3d246327fecc058a6f579c27df
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2025-01-02 15:25:03 +0900

    update

--- jelly_fpga_client/jelly_fpga_control.py
+++ jelly_fpga_client/jelly_fpga_control.py
@@ -15,6 +15,18 @@ class JellyFpgaControl:
         self.channel = grpc.insecure_channel(target)
         self.stub = jelly_fpga_control_pb2_grpc.JellyFpgaControlStub(self.channel)
 
+    def reset(self):
+        response = self.stub.Reset(jelly_fpga_control_pb2.ResetRequest())
+        return response.result
+
+    def load(self, name):
+        response = self.stub.Load(jelly_fpga_control_pb2.LoadRequest(name=name))
+        return response.result
+
+    def unload(self):
+        response = self.stub.Unload(jelly_fpga_control_pb2.UnloadRequest())
+        return response.result
+
     def upload_firmware(self, name, data, chunk_size=2*1024*1024):
         def generate_message(name, data):
             while len(data) > 0:
@@ -23,7 +35,7 @@ class JellyFpgaControl:
         response = self.stub.UploadFirmware(generate_message(name, data))
         return response.result
 
-    def upload_firmware_file(self, path, name, chunk_size=2*1024*1024):
+    def upload_firmware_file(self, name, path, chunk_size=2*1024*1024):
         with open(path, "rb") as f:
             self.upload_firmware(name, f.read(), chunk_size)
 
@@ -31,12 +43,26 @@ class JellyFpgaControl:
         response = self.stub.RemoveFirmware(jelly_fpga_control_pb2.RemoveFirmwareRequest(name=name))
         return response.result
 
-    def load_bitstream(self, name):
-        response = self.stub.LoadBitstream(jelly_fpga_control_pb2.LoadBitstreamRequest(name=name))
-        return response.result
+    def dts_to_dtb(self, dts):
+        response = self.stub.DtsToDtb(jelly_fpga_control_pb2.DtsToDtbRequest(dts=dts))
+        if not response.result:
+            return None
+        return response.dtb
     
     def bitstream_to_bin(self, bitstream_name, bin_name, arch):
         response = self.stub.BitstreamToBin(jelly_fpga_control_pb2.BitstreamToBinRequest(bitstream_name=bitstream_name, bin_name=bin_name, arch=arch))
+        return response.result
+
+    def load_dtbo(self, name):
+        response = self.stub.LoadDtbo(jelly_fpga_control_pb2.LoadDtboRequest(name=name))
+        return response.result
+
+    def load_bin(self, name):
+        response = self.stub.LoadBitstream(jelly_fpga_control_pb2.LoadBitstreamRequest(name=name))
+        return response.result
+
+    def load_bitstream(self, name):
+        response = self.stub.LoadBitstream(jelly_fpga_control_pb2.LoadBitstreamRequest(name=name))
         return response.result
 
     def open_mmap(self, path, offset, size, unit=0):
